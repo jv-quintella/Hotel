d0305c45a226bb230a474d27644df3e48ec988d9
LeopZ3006 <86243491+LeopZ3006@users.noreply.github.com>
2026-05-26 15:57:14 -0300

    Add check-in and reservation cancellation functions

--- JLLR-Hotel-Ativ04.c
+++ JLLR-Hotel-Ativ04.c
@@ -24,3 +24,60 @@ void AGA[i][j] //Função parte 5 checkout.
         }
 		AGA[i][j] = '.';
 }
+
+
+void fSemResCheckIn(int q[i][j])  //Função parte 4 checkin sem reserva.
+{
+    printf("\nDigite o apartamento para o checkout (0 0 para sair): ");
+		scanf("%d %d", &i, &j);
+		if (i == 0 || j == 0)
+			break;
+
+		if(i < 1 || i > 20 || j < 1 || j > 14)
+		{
+			printf("Apt. invalida\n");
+			system("pause");
+			continue;
+		}
+        if(SemResCheckIn[i][j] != '.')
+        {
+            printf("Este apartamento já está ocupado\n");
+            system("pause");
+            continue;
+        }
+		SemResCheckIn[i][j] = 'O';
+}
+
+
+void fCancelarRes (int q[i][j])     //Req04 cancelar reserva
+{
+    int i, j;
+    printf("Qual o quarto da reserva que deseja cancelar?");
+    scanf("%d %d", i, j);
+    if (i == 0 || j == 0)
+			break;
+
+    if(i < 1 || i > 20 || j < 1 || j > 14)
+		{
+			printf("Apt. invalida\n");
+			system("pause");
+			continue;
+		}
+    	
+    if(i < 1 || i > 20 || j < 1 || j > 14)
+		{
+			printf("Apt. invalida\n");
+			system("pause");
+			continue;
+		}
+
+    if(fCancelarRes[i][j] != 'R')
+        {
+            printf("Este apartamento já está ocupado\n");
+            system("pause");
+            continue;
+        }
+		fCancelarRes[i][j] = '.';
+    
+
+}
