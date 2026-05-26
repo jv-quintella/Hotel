6f77ed1b20d2c6845d060bcd82efd2cb74932326
LuisBenttley1973 <fefarinhamarchi@gmail.com>
2026-05-26 16:54:49 -0300

    Add files via upload

---
+++ Ativ04-Hotel-Matriz.cpp
@@ -0,0 +1,46 @@
+// Matriz para Atividade-Hotel
+ 
+#include <stdio.h>
+#include <iostream>
+int main()
+{
+	char AGA[21][15];
+	int i, j;
+	for(i = 20; i >= 1; i--)
+	{
+		for(j=14; j >= 1; j--)
+		{
+			AGA[i][j] = '.';
+		}	
+	}
+	do
+	{
+		system("cls");
+		printf("Apartamentos->  ");
+		for(j=1; j < 15; j++)
+		{
+			printf("%3d", j);
+		}
+		printf("\n\n");
+		for (i = 20; i >= 1; i--)
+		{
+			printf("Andar%3d\t", i);
+			for(j=1; j < 15; j++)
+			{
+				printf("%3c", AGA[i][j]);
+			}
+			printf("\n");
+		}
+		printf("\nPor favor, informe qual andar e qual apartamento deseja hospedar/reservar, respectivamente: ");
+		scanf("%d %d", &i, &j);
+		if (i == -1 || j == -1)
+			break;
+		if(i < 0 || i >20 || j < 0 || j > 14)
+		{
+			printf("Andar ou Apartamento invalido, por favor tente novamente.\n");
+			system("pause");
+			continue;
+		}
+		AGA[i][j] = 'R';
+	}while(i != -1 || j != -1);
+}
